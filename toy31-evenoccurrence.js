0e3e8c76ad6345596689682dc21f454babfbfe14
Ethan Rubio <ethanrubio@gmail.com>
2016-03-09 10:05:59 -0800

    refactors code to remove quadratic time

--- toy31-evenoccurrence.js
+++ toy31-evenoccurrence.js
@@ -1,16 +1,14 @@
 function evenOccurrence (arr) {
   // plain english
   // 1. iterate through array
-  // 2. grab each item and push each duplicate into another arary of arrays
-  // 3. look to see which length is even
+  // 2. grab each item and create a prop on an obj
+  // 3. look to see which value on the obj is even
   // 4. return first letter of first occurence of even
 
-  var duplicateHolder = [];
   var duplicateCounter = {};
 
   for (var i = 0; i < arr.length; i++) {
-    if (duplicateHolder.indexOf(arr[i]) === -1) {
-      duplicateHolder.push(arr[i]);
+    if (!duplicateCounter[arr[i]]) {
       duplicateCounter[arr[i]] = 1;
     } else {
       duplicateCounter[arr[i]] += 1;
